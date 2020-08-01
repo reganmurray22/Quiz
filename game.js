969431a212431b459bafc985d8a3ea2e8347a0a4
reganmurray22 <67241915+reganmurray22@users.noreply.github.com>
2020-08-01 13:25:15 -0500

    correct/incorrect working, timer subtracting!

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ let progress = score + " / " + questionCounter;
 
 document.getElementById("question-counter").textContent = progress;
  
-let secondsRemaining = 30;
+let secondsRemaining = 60;
 function countdown() {
     setInterval (function () {
         if (secondsRemaining < 0) {
@@ -26,49 +26,49 @@ var questions = [
     {
     question: "What is the correct syntax for referring to an external javascript file called 'xxx.js'?",
     answers: ["<script href='xxx.js'>", "<script url='xxx.js'>", "<script src='xxx.js'>"],
-    solution: "3",
+    solution: 3,
     },
 
     {
     question: "How to write an ‘if’ statement for executing some code if “i” is NOT equal to 5?",
     answers: ["if(i/=5) ", "if i!=5", "if(i!=5)"],
-    solution: "3",
+    solution: 3,
     },
 
     { 
     question: "Which of the following is NOT a reserved word in JavaScript?",
     answers: ["fix", "break", "return"],
-    solution: "1", 
+    solution: 1, 
     },
 
     { 
     question: "What is the syntax for creating a function in JavaScript?",
     answers: ["function:()", "function()", "function = ()"],
-    solution: "2",
+    solution: 2,
     },
 
     { 
     question: "What is the correct syntax for adding comments in JavaScript?",
     answers: ["**comments**", "<!--comments-->", "//comments"],
-    solution:"3",
+    solution: 3,
     },
 
     { 
     question: "How do you initialize an array in JavaScript?",
     answers: ["var array = ['A', 'B', 'C']", "var array = (A, B, C)", "var array = 'A', 'B', 'C'"],
-    solution: "1",
+    solution: 1,
     },
 
     {
     question: "String values must be enclosed within ___________ when being assigned to variables.",
     answers: ["Curly Brackets", "Quotes", "Parenthesis"],
-    solution: "2",
+    solution: 2,
     },
 
     {
     question: "A very useful tool used during development and debugging for printing content to the debugger is:",
     answers: ["console.log", "gitHub", "Terminal/Bash"],
-    solution: "1",
+    solution: 1,
     },
 ]
 
@@ -89,19 +89,21 @@ function getQuestion () {
 
 
 //incorrect and correct
-
 function Random(id) {
-        var choice = parseInt(document.getElementById (id).value);
-        if (choice == questions[x].solution) {                    
-            document.getElementsByClassName("choice-text").style.border = "green";
-            document.getElementById ("display").innerHTML = "Correct!";
-            score += 1;
-        } else {
-            document.getElementsByClassName("choice-text").style.border = "red";
-            secondsRemaining -= 5;                       
-            document.getElementById ("display").innerHTML = "Incorrect!";            
-        }
+    var choice = parseInt(document.getElementById (id).value);
+    if (choice == questions[x].solution) {  
+        console.log(choice);
+        console.log(questions[x].solution);                  
+        document.getElementById ("display").innerHTML = "Correct!";
+        score += 1;
+    } else {  
+        console.log(choice);
+        console.log(questions[x].solution);          
+        secondsRemaining -= 5;                       
+        document.getElementById ("display").innerHTML = "Incorrect!";            
+    }
 }
+ 
 
 getQuestion();
     
